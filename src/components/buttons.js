67324e8c0c2518f7b6c0b0ba59f17e2b3d4772ed
Florien Niyongabo <waka@Floriens-MacBook-Pro-40797.local>
2024-11-06 18:02:37 +0200

    ft(Button width)

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -3,7 +3,7 @@ import React from "react";
 export const SecondaryButton = (props) => {
   return (
     <button
-      className={`flex items-center gap-2 w-[120px] md:w-fit h-8 border text-xs text-primary cursor-pointer rounded-md p-2 md:p-4 md:text-sm ${
+      className={`flex items-center gap-2 w-[150px] lg:w-fit h-8 border text-xs text-primary cursor-pointer rounded-md p-2 md:p-4 md:text-sm ${
         props.currentTheme === "light"
           ? "bg-content-bg border-secondary text-secondary"
           : "bg-primary/20 text-white cursor-pointer border-none"
